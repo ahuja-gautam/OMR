2ca8e0f8f9139c064837a6f636453b94ca3e54c7
ahuja-gautam <47530525+ahuja-gautam@users.noreply.github.com>
2019-07-09 12:03:35 +0530

    Updated Sheet Design

--- Unnayan_Checker.py
+++ Unnayan_Checker.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[58]:
+# In[33]:
 
 
 from imutils import contours
@@ -19,7 +19,7 @@ import os
 import math
 
 
-# In[59]:
+# In[34]:
 
 
 present_dir=os.getcwd()
@@ -29,20 +29,20 @@ save_dir=present_dir+'\RESULTS'
 print(save_dir)
 
 
-# In[60]:
+# In[35]:
 
 
 import glob
 images = [cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB) for file in glob.glob(image_dir+r'\*png')]
 
 
-# In[61]:
+# In[36]:
 
 
 len(images)
 
 
-# In[62]:
+# In[37]:
 
 
 def prepare_answer_key(num_q):
@@ -56,7 +56,7 @@ def prepare_answer_key(num_q):
     return ANSWER_KEY
 
 
-# In[63]:
+# In[38]:
 
 
 def remove_shadows(gray_scale):
@@ -75,7 +75,7 @@ def remove_shadows(gray_scale):
     return cv2.merge(result_norm_planes)
 
 
-# In[64]:
+# In[39]:
 
 
 def get_answers(questionCnts, thresh, ANSWER_KEY,paper):
@@ -121,13 +121,13 @@ def get_answers(questionCnts, thresh, ANSWER_KEY,paper):
     return ans_array
 
 
-# In[65]:
+# In[40]:
 
 
 def get_master(TopBox, thresh):   
     Master=[]
-    for i in range(0,120,12):
-        cnts=contours.sort_contours(TopBox[i:i+12])[0]
+    for i in range(0,130,13):
+        cnts=contours.sort_contours(TopBox[i:i+13])[0]
         for(j, c) in enumerate(cnts):            
             mask = np.zeros(thresh.shape, dtype="uint8")
             cv2.drawContours(mask, [c], -1, 255, -1)
@@ -143,7 +143,7 @@ def get_master(TopBox, thresh):
     
 
 
-# In[66]:
+# In[41]:
 
 
 def get_roll_number(ROLL_NO, thresh):
@@ -182,7 +182,7 @@ def get_roll_number(ROLL_NO, thresh):
     return roll, X_encounter
 
 
-# In[67]:
+# In[42]:
 
 
 def find_questions(thresh, paper):
@@ -202,7 +202,7 @@ def find_questions(thresh, paper):
     return questionCnts
 
 
-# In[68]:
+# In[43]:
 
 
 ##below function for precision
@@ -224,7 +224,7 @@ def find_questions_precise(thresh, paper):
     return questionCnts
 
 
-# In[69]:
+# In[44]:
 
 
 def shuffle_answers(answer, num_q):
@@ -245,7 +245,7 @@ def shuffle_answers(answer, num_q):
         
 
 
-# In[70]:
+# In[45]:
 
 
 def find_edges(edged):
@@ -277,71 +277,72 @@ def find_edges(edged):
     return docCnt
 
 
-# In[77]:
+# In[46]:
 
 
 def get_details(master):
     school_id=['X', 'X', 'X', 'X', 'X']
     class_id=['X', 'X']
-    section_id=['X']
+    section_id=['X', 'X']
     roll_no=['X', 'X']
     test_id=['X', 'X']
     
+    step=13
     for offset in range(0,5):
         flag=False
-        for i in range(0+offset, 109+offset, 12):
+        for i in range(0+offset, 118+offset, step):
             print(i, master[i])
             if(master[i]==1 and flag==True):
                 school_id[offset]='X'
                 break
             if(master[i]==1):
                 flag=True
-                school_id[offset]=math.floor((i-offset)/12)
+                school_id[offset]=math.floor((i-offset)/step)
     
     for offset in range(0,2):
         flag=False
-        for i in range(5+offset, 114+offset, 12):
-            if(master[i]==1 and flag==True):
-                roll_no[offset]='X'
-                break
-            if(master[i]==1):
-                flag=True
-                roll_no[offset]=math.floor((i-5-offset)/12)
-    
-    for offset in range(0,2):
-        flag=False
-        for i in range(7+offset, 115+offset, 12):
-            if(master[i]==1 and flag==True):
-                test_id[offset]='X'
-                break
-            if(master[i]==1):
-                flag=True
-                test_id[offset]=math.floor((i-7-offset)/12)
-                
-    for offset in range(0,2):
-        flag=False
-        for i in range(9+offset, 118+offset, 12):
+        for i in range(5+offset, 123+offset, step):
             if(master[i]==1 and flag==True):
                 class_id[offset]='X'
                 break
             if(master[i]==1):
                 flag=True
-                class_id[offset]=math.floor((i-9-offset)/12)
-                
-    for offset in range(0,1):
+                class_id[offset]=math.floor((i-5-offset)/step)
+    
+    for offset in range(0,2):
         flag=False
-        for i in range(11+offset, 120+offset, 12):
+        for i in range(7+offset, 125+offset, step):
             if(master[i]==1 and flag==True):
                 section_id[offset]='X'
                 break
             if(master[i]==1):
                 flag=True
-                section_id[offset]=chr(math.floor((i-10-offset)/12)+ord('A'))
+                section_id[offset]=math.floor((i-7-offset)/step)
+                
+    for offset in range(0,2):
+        flag=False
+        for i in range(9+offset, 127+offset, step):
+            if(master[i]==1 and flag==True):
+                roll_no[offset]='X'
+                break
+            if(master[i]==1):
+                flag=True
+                roll_no[offset]=math.floor((i-9-offset)/step)
+                
+    for offset in range(0,2):
+        flag=False
+        for i in range(11+offset, 129+offset, step):
+            if(master[i]==1 and flag==True):
+                test_id[offset]='X'
+                break
+            if(master[i]==1):
+                flag=True
+                test_id[offset]=math.floor((i-11-offset)/step)
                 
     return school_id, roll_no, test_id, class_id, section_id
 
 
-# In[78]:
+# In[47]:
 
 
 def checkX(school_id, class_id, section_id, roll_no, test_id):
@@ -363,11 +364,11 @@ def checkX(school_id, class_id, section_id, roll_no, test_id):
     return False
 
 
-# In[79]:
+# In[48]:
 
 
 def evaluate_image_batch(images, num_q):
-    ANSWER_KEY=prepare_answer_key(num_q)
+    ANSWER_KEY=prepare_answer_key(num_q) ##commment this line out when taking input as custom answer key
     SCHOOL_id=[]
     CLASS_id=[]
     SECTION_id=[]
@@ -411,7 +412,7 @@ def evaluate_image_batch(images, num_q):
         
         
         
-        if(ratio>=1.44 or len(questionCnts)!=(120+(4*num_q))):
+        if(ratio>=1.44 or len(questionCnts)!=(130+(4*num_q))):
             SCHOOL_id.append("Flagged")
             CLASS_id.append("Flagged")
             SECTION_id.append("Flagged")
@@ -426,8 +427,8 @@ def evaluate_image_batch(images, num_q):
             
         AllCircles = imutils.contours.sort_contours(questionCnts, method="top-to-bottom")[0]
         
-        TopBox=AllCircles[0:120]
-        questionCnts=AllCircles[120:]
+        TopBox=AllCircles[0:130]
+        questionCnts=AllCircles[130:]
         
         
         
@@ -507,7 +508,7 @@ def evaluate_image_batch(images, num_q):
     return SCHOOL_id, CLASS_id, SECTION_id, ROLL_no, TEST_id, Status, Answers
 
 
-# In[80]:
+# In[49]:
 
 
 SCHOOL, CLASS, SECTION, ROLL, TEST_ID, Status, ANSWERS=evaluate_image_batch(images, 50)
@@ -535,13 +536,13 @@ SCHOOL, CLASS, SECTION, ROLL, TEST_ID, Status, ANSWERS=evaluate_image_batch(imag
 #         warped=resize(warped)
 #         paper=resize(paper)
 
-# In[81]:
+# In[50]:
 
 
 mapping=list(zip(SCHOOL, CLASS, SECTION, ROLL, TEST_ID, Status, ANSWERS))
 
 
-# In[82]:
+# In[51]:
 
 
 import pandas as pd
@@ -549,21 +550,506 @@ df=pd.DataFrame(mapping, columns=['SCHOOL_id','CLASS_id', 'SECTION_id','ROLL', '
 df.head(n=10)
 
 
-# In[57]:
+# In[52]:
 
 
 df.to_csv("Results.csv")
 
 
-# ## Mapping explained
-# mapping[i] details for ith student
+# In[ ]:
+
+
+image = cv2.cvtColor(cv2.imread(r"C:\Users\Gautam\Desktop\OMR\IMAGES\20190622193251_orig.png"), cv2.COLOR_BGR2RGB)
+gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+edged=edges_det(image, 200, 250)
+
+
+# In[ ]:
+
+
+cv2.imwrite("image.jpg", image)
+cv2.imwrite("gray.jpg", gray)
+cv2.imwrite("edged.jpg", edged)
+edged = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, np.ones((5, 11))) ## closing operation after finding edges close the gaps
+
+
+# In[ ]:
+
+
+cv2.imwrite("edgedn.jpg", edged)
+
+
+# In[ ]:
+
+
+conts=find_page_contours(edged, resize(image))
+implt(image)
+image.shape
+
+
+# In[ ]:
+
+
+contoured=cv2.drawContours((image.copy()), [conts], -1, (0, 255, 0), 3)
+
+
+# In[ ]:
+
+
+paper=(cv2.rotate(image,rotateCode=cv2.ROTATE_90_CLOCKWISE))
+warped=(cv2.rotate(gray,rotateCode=cv2.ROTATE_90_CLOCKWISE))
+
+
+# In[ ]:
+
+
+cv2.imwrite("11contouredn.jpg", contoured)
+
+
+# In[ ]:
+
+
+implt(paper)
+
+
+# In[ ]:
+
+
+conts
+
+
+# In[ ]:
+
+
+conts=conts.dot(ratio(image))
+
+
+# In[ ]:
+
+
+cv2.imwrite("5contoured.jpg", contoured)
+
+
+# In[ ]:
+
+
+##paper = persp_transform(image, conts)
+##warped = persp_transform(gray, conts)
+
+
+# In[ ]:
+
+
+##paper=image
+##warped=gray
+
+
+# ## EXPERIMENTAL LINE
+
+# In[ ]:
+
+
+implt(paper)
+
+
+# In[ ]:
+
+
+##  cnts = cv2.findContours(warped.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
+## cv2.drawContours(warped, cnts, -1, (0,255,0), 3)
+
+
+# In[ ]:
+
+
+cv2.imwrite("6paper.jpg", paper)
+cv2.imwrite("7warped.jpg", warped)
+##warped=resize(warped)
+##paper=resize(paper)
+
+
+# In[ ]:
+
+
+implt(warped)
+
+
+# ratio=(warped.shape[1]/warped.shape[0])
+# ratio=np.int0(ratio)
+# upsampled=np.ones(warped.shape)
+# upsampled=cv2.pyrUp(warped, upsampled)
+
+# upsampled.shape
+# implt(upsampled)
+
+# In[ ]:
+
+
+def remove_shadows(gray_scale):
+    rgb_planes=cv2.split(gray_scale)
+    result_planes = []
+    result_norm_planes = []
+    for plane in rgb_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        norm_img=np.ones(shape=diff_img.shape)
+        norm_img=cv2.normalize(diff_img,norm_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+        result_planes.append(diff_img)
+        result_norm_planes.append(norm_img)
+    result = cv2.merge(result_planes)
+    return cv2.merge(result_norm_planes)
+
+
+# In[ ]:
+
+
+warped=remove_shadows(warped)
+implt(warped)
+
+
+# In[ ]:
+
+
+warpedcopy=cv2.resize(warped, (430,600))
+papercopy=cv2.resize(paper, (430,600))
+
+
+# In[ ]:
+
+
+thresh = cv2.threshold(warpedcopy, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+
+# In[ ]:
+
+
+## thresherode=cv2.erode(thresh, np.ones((3,3), np.uint8))
+## threshopen=cv2.dilate(thresherode, np.ones((3,3), np.uint8))
+
+
+# In[ ]:
+
+
+print(thresh.shape[1])
+thresh.shape[0]
+
+
+# In[ ]:
+
+
+cv2.imwrite("13eroded.jpg", thresherode)
+
+
+# ratio=(thresh.shape[1]/thresh.shape[0])
+# upsampled=np.ones(thresh.shape)
+# upsampled=cv2.pyrUp(thresh, upsampled, (800, 594))
+
+# upsampled.shape
+
+# implt(upsampled)
+
+# In[ ]:
+
+
+cv2.imwrite("8thresh.jpg", thresh)
+
+
+# In[ ]:
+
+
+implt(thresh)
+
+
+# In[ ]:
+
+
+def find_questions(thresh, paper):
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    questionCnts = []
+    tester=cv2.drawContours(paper.copy(), cnts, -1, (255,0,0), 2)
+
+    for c in cnts:
+
+        (x, y, w, h) = cv2.boundingRect(c)
+        ar = w / float(h)
+        approx = cv2.approxPolyDP(c,0.01*cv2.arcLength(c,True),True)
+        area = cv2.contourArea(c)
+        print(w, h, ar, len(approx), area)
+        if ((w >= 12 and h >=12) and (w<=16 and h<=16) and (area>90 and area<200) and len(approx)>=8 and ar >= 0.8 and ar <= 1.4):
+            questionCnts.append(c)
+    return questionCnts
+
+
+# In[ ]:
+
+
+test_conts=cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+test_conts=imutils.grab_contours(test_conts)
+tester=cv2.drawContours(paper.copy(), test_conts, -1, (255,0,0), 2)
+cv2.imwrite("tester.jpg", tester)
+AllTests=imutils.contours.sort_contours(test_conts, method="top-to-bottom")[0]
+xyz=[]
+for c in AllTests:    
+    (x, y, w, h) = cv2.boundingRect(c)
+    approx = cv2.approxPolyDP(c,0.01*cv2.arcLength(c,True),True)
+    area = cv2.contourArea(c)
+    print(w,h,w/float(h),len(approx), area)
+        
+
+
+# In[ ]:
+
+
+questionCnts=find_questions(thresh, paper)
+AllCircles = imutils.contours.sort_contours(questionCnts, method="top-to-bottom")[0]
+correct = 0
+
+maxm = []
+##ans_array=[]
+##roll=[]
+
+
+# In[ ]:
+
+
+tester2=cv2.drawContours(paper.copy(), xyz, -1, (255,0,0), 2)
+cv2.imwrite("test2.jpg", tester2)
+
+
+# In[ ]:
+
+
+ROLL_NO=AllCircles[0:80]
+questionCnts=AllCircles[80:]
+
+
+# In[ ]:
+
+
+questionCnts=imutils.contours.sort_contours(questionCnts, method="top-to-bottom")[0]
+
+
+# In[ ]:
+
+
+num_q=30
+
+
+# In[ ]:
+
+
+print(len(ROLL_NO))
+print(len(questionCnts))
+if(len(ROLL_NO)!=80):
+    raise Exception("All Roll numbers not recognised, please take pic again")
+if(len(questionCnts)!=(num_q*4)):
+    raise Exception("All questions not recognised, please take pic again")
+
+
+# In[ ]:
+
+
+cv2.drawContours(cv2.resize(paper, (430,600)), ROLL_NO, -1, (255,0,100), 3)
+cv2.imwrite("12rolls.jpg", paper)
+
+
+# ## DEBUGGING CELL
+
 # 
-# mapping[i][0] is the roll number of ith student in list form
 # 
-# mapping[i][1] returns answer array of ith student 
+# 
+# for (q, i) in enumerate(np.arange(0, len(questionCnts), 4)):
+# 	count+=1
+# 	# sort the contours for the current question from
+# 	# left to right, then initialize the index of the
+# 	# bubbled answer
+# 	cnts = contours.sort_contours(questionCnts[i:i + 4])[0]
+# 	bubbled = None
+# 	flag=False
+# 	# loop over the sorted contours
+# 	for (j, c) in enumerate(cnts):
+# 		# construct a mask that reveals only the current
+# 		# "bubble" for the question
+# 		mask = np.zeros(thresh.shape, dtype="uint8")
+# 		cv2.drawContours(mask, [c], -1, 255, -1)
+# 
+# 		# apply the mask to the thresholded image, then
+# 		# count the number of non-zero pixels in the
+# 		# bubble area
+# 		mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+# 		total = cv2.countNonZero(mask)
+# 
+# 		minm.append(total)
+#           
+# 		# if the current total has a larger number of total
+# 		# non-zero pixels, then we are examining the currently
+# 		# bubbled-in answer
+# 		if bubbled is None or total > bubbled[0]:
+# 			bubbled = (total, j) # What ???
+# 	maxm.append(bubbled[0])
+# 	# initialize the contour color and the index of the
+# 	# *correct* answer
+# 	color = (0, 0, 255)
+# 	k = ANSWER_KEY[q]
+# 	if k == bubbled[1]:
+# 			color = (0, 255, 0)
+# 			correct += 1        
+# 	# check to see if the bubbled answer is correct 
+# 	# draw the outline of the correct answer on the test
+# 	cv2.drawContours(paper, [cnts[k]], -1, color, 3)
 
-# In[20]:
+# print(minm)
+# for o in minm:
+#     maxm.remove(o)
+# print(maxm)
+# 
+
+# In[ ]:
+
+
+minm=[]
+maxm=[]
+
+
+# In[ ]:
+
+
+def get_answers(questionsCnts, thresh, ANSWER_KEY, ans_array):
+    for (q, i) in enumerate(np.arange(0, len(questionCnts), 4)):
+        
+
+        cnts = contours.sort_contours(questionCnts[i:i + 4])[0]
+        bubbled = None
+        flag=False
+
+        for (j, c) in enumerate(cnts):
+
+            mask = np.zeros(thresh.shape, dtype="uint8")
+            cv2.drawContours(mask, [c], -1, 255, -1)
+
+
+            mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+            total = cv2.countNonZero(mask)
+            minm.append(total)
+            if (flag and total>700): ##was 220
+                bubbled=None
+                break
+            if(total>700):
+                flag=True
+
+            if(total>700):
+                bubbled=(total,j)
+        
+        color = (0, 0, 255)
+        k = ANSWER_KEY[q]
+
+        if bubbled and k == bubbled[1]:
+            color = (0, 255, 0)
+            ans_array.append(k)       
+        elif not bubbled:
+            ans_array.append("NA")
+        elif bubbled and k!=bubbled[1]:
+            ans_array.append(bubbled[1])  
+
+        cv2.drawContours(papercopy, [cnts[k]], -1, color, 3)
+    return ans_array
+
+
+# In[ ]:
+
+
+def get_roll_number(ROLL_NO, thresh):
+    roll=[]
+    count=-1;
+    idx=0
+    for i in range(0,80, 10):
+        count+=1;
+        cnts = contours.sort_contours(ROLL_NO[i:i + 10])[0]
+        bubbled = None
+        flag=False
+        for (j, c) in enumerate(cnts):
+
+            mask = np.zeros(thresh.shape, dtype="uint8")
+            cv2.drawContours(mask, [c], -1, 255, -1)
+
+            mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+            total = cv2.countNonZero(mask)
+            maxm.append(total)
+            if (flag and total>700):
+                bubbled=None
+                break
+            if(total>700):
+                flag=True
+            if(total>700):
+                bubbled=(total,j)
+            color = (0, 255, 255)
+        if(bubbled):
+            roll.append(bubbled[1])
+        else:
+            roll.append("X")
+         
+    return roll
+
+
+# In[ ]:
+
+
+questions=cv2.drawContours(warpedcopy.copy(), questionCnts, -1, (0,255,0), 3)
+
+
+# In[ ]:
+
+
+cv2.imwrite("9questions.jpg", questions)
+
+
+# In[ ]:
+
+
+implt(questions)
+
+
+# In[ ]:
 
 
 
+ans_array=[]
+ans_array=get_answers(questionCnts, thresh, ANSWER_KEY, ans_array)
+roll=get_roll_number(ROLL_NO, thresh)
+print(ans_array)
+print(len(ans_array))
+print(roll)
+
+
+# In[ ]:
+
+
+score = (correct / len(questionCnts)) * 100
+print(correct)
+print("[INFO] Score: {:.0f}%".format(score))
+cv2.putText(paper, "{:.0f}%".format(score), (10, 30),
+cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+cv2.imshow("Result", papercopy)
+cv2.imwrite("Result.jpg", papercopy)
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+def write_to_csv(ans_array, filename):
+    file=open(filename, "w+")
+    for x in ans_array:
+        file.write(str(x))
+        file.write(',')
+    file.close()    
+
+
+# In[ ]:
+
+
+write_to_csv(ans_array, "abc.txt")
 
